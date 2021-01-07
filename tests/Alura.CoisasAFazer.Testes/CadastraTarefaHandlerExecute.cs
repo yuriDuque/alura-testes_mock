1e3fd54938b03255d277ee42f3cae1fb148c7413
yuriDuque <yurithielmann83@gmail.com>
2021-01-07 08:10:28 -0300

    enviando projeto

---
+++ tests/Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
@@ -0,0 +1,30 @@
+using Alura.CoisasAFazer.Core.Commands;
+using Alura.CoisasAFazer.Core.Models;
+using Alura.CoisasAFazer.Services.Handlers;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Alura.CoisasAFazer.Testes
+{
+    public class CadastraTarefaHandlerExecute
+    {
+        [Fact]
+        public void DadaTarefaComInfoValidasDeveIncluirNoBD()
+        {
+            //arrange
+            var comando = new CadastraTarefa("Estudar Xunit", new Categoria("Estudo"), new DateTime(2019, 12, 31));
+
+            var repo = new RepositorioFake();
+
+            var handler = new CadastraTarefaHandler(repo);
+
+            //act
+            handler.Execute(comando); //SUT >> CadastraTarefaHandlerExecute
+
+            //assert
+            var tarefa = repo.ObtemTarefas(t => t.Titulo == "Estudar Xunit").FirstOrDefault();
+            Assert.NotNull(tarefa);
+        }
+    }
+}
