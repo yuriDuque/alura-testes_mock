8b00f686a9bcb705bf100494e7eb141e6d989c5d
yuriDuque <yurithielmann83@gmail.com>
2021-01-19 08:05:01 -0300

    lancando exception nos testes

--- tests/Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
+++ tests/Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
@@ -3,6 +3,7 @@ using Alura.CoisasAFazer.Core.Models;
 using Alura.CoisasAFazer.Infrastructure;
 using Alura.CoisasAFazer.Services.Handlers;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using System;
 using System.Linq;
 using Xunit;
@@ -31,6 +32,28 @@ namespace Alura.CoisasAFazer.Testes
             //assert
             var tarefa = repo.ObtemTarefas(t => t.Titulo == "Estudar Xunit").FirstOrDefault();
             Assert.NotNull(tarefa);
+        }
+
+        [Fact]
+        public void QuandoExceptionForLancadaResultadoIsSuccessDeveSerFalse()
+        {
+            // Arrange
+            var comando = new CadastraTarefa("Estudar Xunit", new Categoria("Estudo"), new DateTime(2019, 12, 31));
+
+            var mock = new Mock<IRepositorioTarefas>();
+
+            mock.Setup(r => r.IncluirTarefas(It.IsAny<Tarefa[]>()))
+                .Throws(new Exception("'Houve um erro ao incluir as tarefas"));
+
+            var repo = mock.Object;
+
+            var handler = new CadastraTarefaHandler(repo);
+
+            // Act
+            CommandResult resultado = handler.Execute(comando);
+
+            // Assert
+            Assert.False(resultado.IsSuccess);
         }
     }
 }
